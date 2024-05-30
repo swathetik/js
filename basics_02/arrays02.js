65173f03d21f4bca56101bdf5df774003a54cab9
Shwet Rana <shwetrana980@gmail.com>
2024-05-30 23:48:34 +0530

    arrays baiscs done

--- basics_02/arrays02.js
+++ basics_02/arrays02.js
@@ -1,5 +1,5 @@
-const marvel_heros = ["spiderman", "ironman", "widdow"];
-const dc_heros = ["superman", "batman", "superwoman"];
+// const marvel_heros = ["spiderman", "ironman", "widdow"];
+// const dc_heros = ["superman", "batman", "superwoman"];
 
 // marvel_heros.push(dc_heros);
 // console.log(marvel_heros); //this shows that arrays take anything as the data, even another array itself.
@@ -12,15 +12,25 @@ const dc_heros = ["superman", "batman", "superwoman"];
 
 // spread operator (...{arryay name})
 // this changes the array into individual elements and combine them to one new operator, and to use spread operator we have to apply (...) in the front of an arrays you want to combine.
-const all_new_heros = [...marvel_heros, ...dc_heros];
+// const all_new_heros = [...marvel_heros, ...dc_heros];
 // console.log(all_new_heros);
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
-const usable_another_Array = another_array.flat(Infinity); // flat operator Returns a new array with all sub-array elements concatenated into the new array.
-console.log(usable_another_Array);
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+// const usable_another_Array = another_array.flat(Infinity); // flat operator Returns a new array with all sub-array elements concatenated into the new array.
+// console.log(usable_another_Array);
+
+/// From and isArray Exmaples
 
 //// isArray checks whether the asked one is array or not.
-console.log(Array.isArray(marvel_heros)); // true
+// console.log(Array.isArray(marvel_heros)); // true
 
-//// from coverts anything into the array as it feels confortable.
-console.log(Array.from("shwet"));
+// //// from coverts anything into the array as it feels confortable.
+// console.log(Array.from("shwet"));
+
+// console.log(Array.from({ name: "shwet" })); //this directly doesn't change the object into the array, you have to tell that whether you want array of keys or of values.
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// console.log(Array.of(score1, score2, score3)); // of method takes the elements and converts it into the array.
