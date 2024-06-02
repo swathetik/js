41ed47c48d62a385d1dd545a195fd7bf556da7e7
Shwet Rana <shwetrana980@gmail.com>
2024-06-02 15:15:25 +0530

    iife in js

---
+++ basics_03/iife.js
@@ -0,0 +1,20 @@
+// Immediatly Ivoked Function Expressions (IIFE).
+//to remove the global pollution from the function, we use IIFE
+// Syntax
+
+// (function chai() {
+//   // this is called named IIFE
+//   console.log("shwet rana");
+// })(); //this is how we call nromal function.
+
+// // to stop this immediatly invoked function we have to add the semicolon.
+
+// (() => {
+//   // this is simple IIFE
+//   console.log(`db connected`);
+// })(); //this is how we call arrow function.
+
+// ((name) => {
+//   // this is simple IIFE
+//   console.log(`db connected for user ${name}`);
+// })("Shwet"); //this is how we call arrow function.
