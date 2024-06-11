e50703cd0057a73f27afbf07e261b758dc20755d
Shwet Rana <shwetrana980@gmail.com>
2024-06-11 15:14:40 +0530

    switch

---
+++ control_flow_04/switch.js
@@ -0,0 +1,32 @@
+// const key = 4;
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default: // this is executed if the given statement doesn't match with among cases.
+//         break;
+// }
+
+//
+
+// const month = 4; //more like an IF/ELSE statement but we don't use it that much
+// switch (month) {
+//   case 1:
+//     console.log("Jan");
+//     break;
+//   case 2:
+//     console.log("feb");
+//     break;
+//   case 3:
+//     console.log("mar");
+//     break;
+//   case 4:
+//     console.log("april");
+//     break;
+
+//   default:
+//     console.log("not in the above cases");
+//     break;
+// }
