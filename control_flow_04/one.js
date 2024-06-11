91a812d8fec85dfcf3de15c727505834b6111a84
Shwet Rana <shwetrana980@gmail.com>
2024-06-11 14:05:11 +0530

    revising basics

---
+++ control_flow_04/one.js
@@ -0,0 +1,41 @@
+// if
+
+// const isUserLoggedIn = true;
+// // if (false) {
+// if (isUserLoggedIn) {
+//   // the code inside the if condition will only work if the condition is true otherwise it'll never enter the condition itself.
+//   console.log("shwet");
+// }
+
+//  comparision operators
+// >,<,<=,>=, == (this means that we are checking whether both of the sides are equal or not)
+// != (this means that both of the sides are not equal)
+// === (this checks the sides and the type of both sides that their data-types are same or not?) this is also called as strict equal!
+
+// const score = 200;
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`power: ${power}`);
+// }
+
+// console.log(`power: ${power}`);  this will give error because const and let are the blocked scoped variables.
+
+// const marks = 1000;
+//this is called implicit scope. this only works in the single like or we can make it to double line but we have to end the code by semi colon
+// if (marks > 500) console.log("pass"), console.log("pass ho gya"); // this is not a good syntax
+
+// checking two statements in the if conditions
+// const isUserLoggedIn = true;
+// const debitCard = true;
+// // using AND statement (&&)
+// if (isUserLoggedIn && debitCard) {
+//   console.log("user can do the shopping");
+// }
+
+// using OR statement (||)
+
+// const GoogleLogggedIn = false;
+// const LoggedInbyEmail = true;
+// if (GoogleLogggedIn || LoggedInbyEmail) {
+//   console.log("Yes user can do the shopping");
+// }
